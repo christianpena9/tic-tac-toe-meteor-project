3d2d65b404a0d7f0192b6a7254d3177767fedc46
Christian Pena <christianpena9@gmail.com>
2016-05-22 22:53:20 -0400

    created two buttons, one for start and another for reset.

--- client/main.js
+++ client/main.js
@@ -4,4 +4,26 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../imports/startup/client/index.js';
 
+$(document).ready(function() {
 
+	var playerX = 'X';
+	var playerO = 'O';
+
+
+	
+
+
+
+
+	$('table tr td').click(function() {
+
+
+
+
+
+
+	}); // End of table tr td click function
+
+
+
+}); // End of document ready function
