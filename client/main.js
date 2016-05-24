28549adacb82f141615d712f6188776a44e4e8ac
Christian Pena <christianpena9@gmail.com>
2016-05-24 19:41:17 -0400

    created the random number generator from 0 to 1

--- client/main.js
+++ client/main.js
@@ -5,24 +5,48 @@ import './main.html';
 
 $(document).ready(function() {
 
-	var playerX = 'X';
-	var playerO = 'O';
+	const playerX = {
+		value: 'X',
+		turn: false,
+		count: 0
+	};
+	const playerO = {
+		value: 'O',
+		turn: false,
+		count: 0
+	};
 
 
+	$('#startGame').click(function() {
+
+		$('table tr td').click(function() {
+
+			console.log( randomPick(playerX,playerO) );
+
+			if(playerX.turn == true) {
+				writeValue(this,playerX.value);
+			} else if (playerO.turn == true) {
+				writeValue(this,playerO.value);
+			}
+			
 
 
+		}); // End of table tr td click function
 
+		function randomPick(playerX,playerO) {
+			const randomNum = Math.floor((Math.random() * 2) + 1);
+			if(randomNum == 1) {
+				playerX.turn = true;
+				return playerX;
+			} else {
+				playerO.turn = true;
+				return playerO;
+			}
+		}
 
+		function writeValue(element,player) {
+			$(element).text(player);
+		}
 
-	$('table tr td').click(function() {
-
-
-
-
-
-
-	}); // End of table tr td click function
-
-
-
+	}); // End of #startGame click function
 }); // End of document ready function
